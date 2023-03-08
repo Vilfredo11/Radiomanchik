bcf868411d990c864c67fe69220ed7570f3bb7d0
Vilfredo <tardileo1996@gmail.com>
2023-03-08 16:04:44 +0600

    Initial commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,80 @@
+package ru.netology;
+
+public class Radio {
+
+    private int currentNumber;
+    private int currentVolume;
+
+    public int getCurrentNumber() {
+
+        return currentNumber;
+    }
+
+    public int getCurrentVolume() {
+
+        return currentVolume;
+    }
+
+    public void increaseNumber() {
+        if (currentNumber > 0) {
+            currentNumber = currentNumber + 1;
+        }
+        return;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        return;
+    }
+
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void next() {
+        if (currentNumber < 9) {
+            currentNumber = currentNumber + 1;
+        } else {
+            currentNumber = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentNumber > 0) {
+            currentNumber = currentNumber - 1;
+        } else {
+            currentNumber = 9;
+        }
+    }
+
+
+    public void setCurrentNumber(int currentNumber) {
+
+        if (currentNumber > 9) {
+            return;
+        }
+        if (currentNumber < 0) {
+            return;
+
+        }
+        this.currentNumber = currentNumber;
+    }
+
+
+    public void setCurrentVolume(int CurrentVolume) {
+
+        if (CurrentVolume > 10) {
+            return;
+        }
+        if (CurrentVolume < 0) {
+            return;
+        }
+        this.currentVolume = CurrentVolume;
+    }
+
+}
